d6549f060ec505ceb9999e6a35f58fea126f67c3
Zzcnick <zzhen@stuy.edu>
2016-12-02 10:34:38 -0500

    add documentation for parser functions

--- parser.c
+++ parser.c
@@ -9,6 +9,11 @@ void strip(char * in);
 char * parseCommands(char ** cmds, char * in, char ** parent);
 
 // Functions
+/* void strip(char * in)
+ * takes in a string and removes all leading and trailing whitespace (modifies the string)
+ * args: string in
+ * return: <void> -- modifies string
+ */
 void strip(char * in) {
   char * tmp = in;
   char * front = 0;
@@ -28,7 +33,14 @@ void strip(char * in) {
   *in = 0;
 }
 
-// 
+/* void parseCommands(char ** cmd, char * in, char ** parent)
+ * parses a string of input to an array of commands, and modifies the string to allow for future
+ * parsing (splits on semicolons)
+ * args: char ** cmd    - array of strings, representing arguments for execvp 
+ *       char *  in     - input string with unparsed command line arguments
+ *       char ** parent - pointer to in to allow modification
+ * return: char * - returns the new value of in
+ */
 char * parseCommands(char ** cmd, char * in, char ** parent) {
   // Pre-parsing
   char * dest = strsep(&in, ";");
